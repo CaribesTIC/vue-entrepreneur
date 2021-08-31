fe6767193192f8d817a1bf7bad62d8f20bd68887
ejimenez123 <caribestic123@gmail.com>
2021-08-31 14:15:12 -0400

    feat: Loading real API products with axios

--- vue3/vue3-green-apple/src/api/shop.js
+++ vue3/vue3-green-apple/src/api/shop.js
@@ -1,20 +1,23 @@
 /**
  * Mocking client-server processing
  */
-const _products = [
-  { id: 1, title: "iPad 4 Mini", price: 500.01, inventory: 2, photo:'cafe.jpg' },
-  { id: 2, title: "H&M T-Shirt White", price: 10.99, inventory: 10, photo:'cebolla.jpg' },
-  { id: 3, title: "Charli XCX - Sucker CD", price: 19.99, inventory: 5,  photo:'patata.jpg' },
-  { id: 4, title: "Xharli XXX - Xucker CD", price: 29.99, inventory: 3, photo:'pina.jpg'  },
-  { id: 5, title: "oPad 4 oini", price: 500.01, inventory: 2, photo:'cafe.jpg' },
-  { id: 6, title: "R&M R-Shirt White", price: 10.99, inventory: 10, photo:'cebolla.jpg' },
-  { id: 7, title: "Aharli ACX - Aucker CD", price: 19.99, inventory: 5,  photo:'patata.jpg' },
-  { id: 8, title: "Zharli ZXX - Xucker CD", price: 29.99, inventory: 3, photo:'pina.jpg'  }
-];
+ 
+import axios from 'axios'
+ 
+const apiGetProducts = async () => {
+    return await axios.get("http://api.mv.com/api/products");
+};
+
+let _products = [];
 
 export default {
-  getProducts(cb) {
-    setTimeout(() => cb(_products), 100);
+
+  async getProducts(cb) {
+    if (_products.length === 0) {
+      let response = await apiGetProducts();
+      _products = response.data;
+      cb(_products);
+    }        
   },
 
   buyProducts(products, cb, errorCb) {
