0155c5884032b90eb1d407656298b548c0c74500
Oswaldo Graterol <oswaldo.graterol@gmail.com>
2021-09-04 14:58:51 -0400

    fix: tiping error

--- vue3/vue3-green-apple/src/router/index.js
+++ vue3/vue3-green-apple/src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   },
-  {it 
+  { 
     path: '/register',
     name: 'Register',
     // route level code-splitting
